9077d57812b4921010d417085f7b66fbada83a8a
Samit Bhattacharjee <samit.prsl@gmail.com>
2023-05-18 17:19:28 +0530

    Final Touches Preliminary

--- popup.js
+++ popup.js
@@ -1,4 +1,18 @@
 let button = document.getElementById('button');
+let list = document.getElementById('emailist');
+
+chrome.runtime.onMessage.addListener((request,sender,sendresponse)=>{
+    let emails = request.emailCheck;
+   if((emails!=null||emails.length!=0))
+   {
+    emails.forEach((emailing) => {
+        let li = document.createElement('li');
+        li.innerText = emailing;
+        //list.appendChild('li');
+        list.appendChild(li);
+    });
+   }
+})
 
 button.addEventListener("click",async ()=>{
     let [tabs] = await chrome.tabs.query({active : true,currentWindow: true});
@@ -21,6 +35,6 @@ function scrapeEmailsFromPage()
     }
     else
     {
-    alert(emailCheck)
+    chrome.runtime.sendMessage({emailCheck})
 }
 }
